6389956fb3607394749dcdbd711b3380405bb21a
python-game <n.marforio@gmail.com>
2023-06-20 14:52:01 +0200

    done with css

--- src/App.tsx
+++ src/App.tsx
@@ -3,6 +3,7 @@ import "./App.css";
 import Button from "./Components/Button";
 import { useState } from "react";
 
+
 interface ObjectRecipe {
   [meal: string]: any;
 }
@@ -38,56 +39,106 @@ const App: React.FC<{}> = (): any => {
   return (
     <>
       <div className="App">
-        <h2>Feeling Hungry?</h2>
-        <h3>get a random Meal by clicking below</h3>
-        <Button handleClick={handleClick} />
+        <div className="button">
+          <h2>Feeling Hungry?</h2>
+          <h3>get a random Meal by clicking below</h3>
+          <Button handleClick={handleClick} />
+        </div>
+
+        {recipe.meals?.map((meal: ObjectRecipe) => {
+          return (
+            <>
+              <div className="mealTitle">
+                <h3>{meal.strMeal}</h3>
+                <img
+                  key={meal.idMela}
+                  src={meal.strMealThumb}
+                  alt="mealPic"
+                  width={300}
+                  height={300}
+                />
+              <div className="ingridientDiv">
+                <h4>Ingridients:</h4>
+                <p>
+                  {meal.strIngredient1} {meal.strMeasure1}
+                </p>
+                <p>
+                  {meal.strIngredient2} {meal.strMeasure2}
+                </p>
+                <p>
+                  {meal.strIngredient3} {meal.strMeasure3}
+                </p>
+                <p>
+                  {meal.strIngredient4} {meal.strMeasure4}
+                </p>
+                <p>
+                  {meal.strIngredient5} {meal.strMeasure5}
+                </p>
+                <p>
+                  {meal.strIngredient6} {meal.strMeasure6}
+                </p>
+                <p>
+                  {meal.strIngredient7} {meal.strMeasure7}
+                </p>
+                <p>
+                  {meal.strIngredient8} {meal.strMeasure8}
+                </p>
+                <p>
+                  {meal.strIngredient9} {meal.strMeasure9}
+                </p>
+                <p>
+                  {meal.strIngredient10} {meal.strMeasure10}
+                </p>
+                <p>
+                  {meal.strIngredient11} {meal.strMeasure11}
+                </p>
+                <p>
+                  {meal.strIngredient12} {meal.strMeasure12}
+                </p>
+                <p>
+                  {meal.strIngredient13} {meal.strMeasure13}
+                </p>
+                <p>
+                  {meal.strIngredient14} {meal.strMeasure14}
+                </p>
+                <p>
+                  {meal.strIngredient15} {meal.strMeasure15}
+                </p>
+                <p>
+                  {meal.strIngredient16} {meal.strMeasure16}
+                </p>
+                <p>
+                  {meal.strIngredient17} {meal.strMeasure17}
+                </p>
+                <p>
+                  {meal.strIngredient18} {meal.strMeasure18}
+                </p>
+                <p>
+                  {meal.strIngredient19} {meal.strMeasure19}
+                </p>
+                <p>
+                  {meal.strIngredient20} {meal.strMeasure20}
+                </p>
+              </div>
+              </div>
+              <div className="ratio">
+                <h4>Video Recipe</h4>
+                <iframe
+                  id="YoutubeVideo"
+                  key={meal.idMeal}
+                  src={`https://www.youtube.com/embed/${meal.strYoutube}`}
+                  title="YouTube video"
+                ></iframe>
+              </div>
+
+              <span className="instructoinSpan">
+                <h4>Instruction</h4>
+                <p>{meal.strInstructions}</p>
+              </span>
+            </>
+          );
+        })}
       </div>
-      {recipe.meals?.map((meal: ObjectRecipe) => {
-        return (
-          <>
-            <div className="recipeDiv">
-              <h3>{meal.strMeal}</h3>
-            </div>
-            <div className="ratio ratio-16x9">
-              <h4>Video Recipe</h4>
-              <iframe
-                id="YoutubeVideo"
-                key={meal.idMeal}
-                src={`https://www.youtube.com/embed/${meal.strYoutube}`}
-                title="YouTube video"
-              ></iframe>
-            </div>
-            <div className="ingridientDiv">
-              <h4>Ingridients:</h4>
-              <p>{meal.strIngredient1} {meal.strMeasure1}</p>
-              <p>{meal.strIngredient2} {meal.strMeasure2}</p>
-              <p>{meal.strIngredient3} {meal.strMeasure3}</p>
-              <p>{meal.strIngredient4} {meal.strMeasure4}</p>
-              <p>{meal.strIngredient5} {meal.strMeasure5}</p>
-              <p>{meal.strIngredient6} {meal.strMeasure6}</p>
-              <p>{meal.strIngredient7} {meal.strMeasure7}</p>
-              <p>{meal.strIngredient8} {meal.strMeasure8}</p>
-              <p>{meal.strIngredient9} {meal.strMeasure9}</p>
-              <p>{meal.strIngredient10} {meal.strMeasure10}</p>
-              <p>{meal.strIngredient11} {meal.strMeasure11}</p>
-              <p>{meal.strIngredient12} {meal.strMeasure12}</p>
-              <p>{meal.strIngredient13} {meal.strMeasure13}</p>
-              <p>{meal.strIngredient14} {meal.strMeasure14}</p>
-              <p>{meal.strIngredient15} {meal.strMeasure15}</p>
-              <p>{meal.strIngredient16} {meal.strMeasure16}</p>
-              <p>{meal.strIngredient17} {meal.strMeasure17}</p>
-              <p>{meal.strIngredient18} {meal.strMeasure18}</p>
-              <p>{meal.strIngredient19} {meal.strMeasure19}</p>
-              <p>{meal.strIngredient20} {meal.strMeasure20}</p>
-            </div>
-            <span className="instructoinSpan">
-              <h4>Instruction</h4>
-              <p>{meal.strInstructions}</p>
-              <img key={meal.idMela} src={meal.strMealThumb} alt="mealPic" />
-            </span>
-          </>
-        );
-      })}
     </>
   );
 };
